6953a7772ebce40abc9e81a74fdd9016a26e6e8d
phaybein <fabian.m.martinez@gmail.com>
2018-08-26 07:05:18 -0700

    added updated styles RE #nav #helpers #typography

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,23 @@
 $('#year').text(new Date().getFullYear());
 
 
+// Smooth Scrolling
+$('.cf a').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
 
 
 console.log('main loaded...');
